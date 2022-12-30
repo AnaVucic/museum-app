17a908e31a95a2821d5cb0445f71914b56d429fd
Ana Vucic <anavucic007@gmail.com>
2022-12-30 12:31:41 +0100

    first commit

---
+++ database/migrations/2022_12_30_104646_create_ticket_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            
+            $table->decimal('price', 9, 3); //column `price`, 9 - precision, 3 - decimal places
+            $table->string('ticket_comment', 200); // column `ticket_column`, 200 - length
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket');
+    }
+};
---
+++ database/migrations/2022_12_30_105931_create_customer_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            //string columns, 100 - max length
+            $table->String('firstname', 100);
+            $table->String('lastname', 100);
+            $table->String('email', 100);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+};
